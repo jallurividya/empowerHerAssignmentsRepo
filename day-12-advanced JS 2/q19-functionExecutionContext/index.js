f418818e88289955fa7336a068de96480e43efab
jallurividya <jallurividya2002@gmail.com>
2025-11-24 20:13:59 +0530

    assignment

---
+++ day-12-advanced JS 2/q19-functionExecutionContext/index.js
@@ -0,0 +1,12 @@
+var age=23;
+function displayAge(){
+    console.log("Display age",age);
+}
+function changeAge(){
+    age++;
+    console.log("Change age",age)
+}
+
+displayAge();
+changeAge();
+displayAge();
