12c9e7ac8718cc9379c3552b400cfc06512e9006
jallurividya <jallurividya2002@gmail.com>
2025-12-20 16:46:50 +0530

    Video Player

---
+++ Module 3/day-8/q1-videoPlayer/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import VideoPlayer from './VideoPlayer'
+import MultiVideoPlayer from './MultiVideoPlayer'
+
+const App = () => {
+  return (
+    <>
+      <VideoPlayer />
+    </>
+  )
+}
+
+export default App
