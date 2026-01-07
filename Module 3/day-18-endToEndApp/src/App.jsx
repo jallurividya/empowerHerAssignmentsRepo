1aa27e09c3dac4e0021b41c7474bce33df61c782
jallurividya <jallurividya2002@gmail.com>
2026-01-07 20:10:00 +0530

    end to end app assignment

--- Module 3/day-18-endToEndApp/src/App.jsx
+++ Module 3/day-18-endToEndApp/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="*" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route
